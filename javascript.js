0279435be02efbdc96fb8291311ad34abf065afa
tommyknkn <tommyknkn@gmail.com>
2025-12-07 18:57:47 +0100

    Website test

---
+++ javascript.js
@@ -0,0 +1,18 @@
+// meny_mobile/
+jQuery(document).ready(function() {
+	$('body').addClass('js');
+	var $menu = $('#menu'),
+		$menulink = $('.menu-link'),
+		$menuTrigger = $('.has-submenu > a');
+	$menulink.click(function(e) {
+		e.preventDefault();
+		$menulink.toggleClass('active');
+		$menu.toggleClass('active');
+		$('html, body').animate({scrollTop:0}, 'slow');
+	});
+	$menuTrigger.click(function(e) {
+		e.preventDefault();
+		var $this = $(this);
+		$this.toggleClass('active').next('ul').toggleClass('active');
+	});
+});
